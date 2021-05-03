6eee28f385124794ee3af43306435454047a28d9
Osamah-learn <77615959+Osamah-learn@users.noreply.github.com>
2021-05-03 17:16:50 +0300

    Update our App with navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homePage.jsx";
+import { Switch, Route } from "react-router-dom";
+
+const hatsPage = () => {
+  return (
+    <div1>
+      <h1>HatsPage {}</h1>
+    </div1>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={hatsPage} />"
+      </Switch>
     </div>
   );
 }
