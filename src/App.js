8a78ef649e5795354e0534211892790afb39a6fd
Osamah-learn <77615959+Osamah-learn@users.noreply.github.com>
2021-05-18 11:59:52 +0300

    I added storing the data from the gmail singin into Firestor db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from "././pages/shop/shop.component.jsx";
 import Header from "../src/components/header/header.component.jsx";
 import SignInAndSignUp from "../src/pages/SingIn-SignUp/SignIn-SignUp";
 import { Switch, Route } from "react-router-dom";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,20 +13,41 @@ class App extends React.Component {
     this.state = { currentUser: null };
   }
   unsubscribeFromAuth = null;
+
   componentDidMount() {
-   this.unsubscribeFromAuth=auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      //Checking if the userAuth exist inside Firestore by createUserProfileDocument function and if its not then we create newProfile inside the users DB
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          console.log(
+            snapShot.data(
+              this.setState({
+                currentUser: {
+                  id: snapShot.id,
+                  ...snapShot.data(),
+                },
+              })
+            )
+            
+          );
+        });
+        console.log(this.state);
+      } else {
+        this.setState({currentUser:userAuth})
+      }
+
+      
     });
-   }
-   componentWillUnmount() {
+  }
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
- 
+
   render() {
     return (
       <div>
-        <Header  currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />"
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
-import firebase from 'firebase/app'
+import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
 
 const config = {
   apiKey: "AIzaSyCTL0ej-gPazmPo2uOOBhv11PPyazBpLhc",
@@ -10,7 +9,28 @@ const config = {
   storageBucket: "arab-db.appspot.com",
   messagingSenderId: "539079317935",
   appId: "1:539079317935:web:38f3baf7ef50dd3653b75a",
-  measurementId: "G-X2Q6R94GE5"
+  measurementId: "G-X2Q6R94GE5",
+};
+// Storing UserAuth inside the DB if its not exsist
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error While creating User", error.message);
+    }
+  }
+  return userRef;
 };
 
 
@@ -18,9 +38,8 @@ firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
-
+// initilais Google Sign in 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
-export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 export default firebase;
